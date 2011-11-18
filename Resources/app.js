fcc45d7570424ea3f314715d68361fa212dec9d2
Kaique Silva <kaiquesilva@localhost.localdomain>
2011-11-18 15:50:34 -0200

    Data Sync
    - Compilando sem erros.
    - Começar aplicativo

--- Resources/app.js
+++ Resources/app.js
@@ -14,13 +14,13 @@ var win1 = Titanium.UI.createWindow({
 });
 var tab1 = Titanium.UI.createTab({  
     icon:'KS_nav_views.png',
-    title:'Tab 1',
+    title:'Main Tab',
     window:win1
 });
 
 var label1 = Titanium.UI.createLabel({
 	color:'#999',
-	text:'I am Window 1',
+	text:'Hello guy!',
 	font:{fontSize:20,fontFamily:'Helvetica Neue'},
 	textAlign:'center',
 	width:'auto'
---
+++ build/dataSync/src/br/com/visie/datasync/dataSync.java
@@ -0,0 +1,15 @@
+package br.com.visie.datasync;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class dataSync extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
